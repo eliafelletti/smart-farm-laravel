e61020a739eaa75b15670ebb1f76aa457e384015
Dario Macchi <dario.macchi@edu.unife.it>
2024-04-11 11:08:04 +0200

    Add user level to users

---
+++ Smart_Farm_Application/database/migrations/2024_04_11_090139_add_level_column_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('level')->after('password');
+            /*
+            Level:
+            0 -> ADMIN
+            1 -> OWNER
+            2 -> SUPPLIER COMPANY
+            */
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('level');
+        });
+    }
+};
