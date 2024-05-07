5fe00549d7df249f60e71678b79c90e19609da8f
Dario Macchi <dario.macchi@edu.unife.it>
2024-05-07 16:34:13 +0200

    Dump database updated

--- dumps_mysql/dump_smart_farm.sql
+++ dumps_mysql/dump_smart_farm.sql
@@ -86,7 +86,7 @@ CREATE TABLE `green_houses` (
   PRIMARY KEY (`id`),
   KEY `green_houses_id_smart_farm_foreign` (`id_smart_farm`),
   CONSTRAINT `green_houses_id_smart_farm_foreign` FOREIGN KEY (`id_smart_farm`) REFERENCES `smart_farms` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,7 +95,7 @@ CREATE TABLE `green_houses` (
 
 LOCK TABLES `green_houses` WRITE;
 /*!40000 ALTER TABLE `green_houses` DISABLE KEYS */;
-INSERT INTO `green_houses` VALUES (25,'45',8,'2024-04-25 07:48:12','2024-04-25 07:48:12'),(26,'25',9,'2024-04-25 07:48:17','2024-04-25 07:48:17'),(27,'15',8,'2024-04-25 07:48:21','2024-04-25 07:48:21'),(45,'30',9,'2024-04-25 15:50:01','2024-04-25 15:50:01');
+INSERT INTO `green_houses` VALUES (25,'45',8,'2024-04-25 07:48:12','2024-04-25 07:48:12'),(26,'35',9,'2024-04-25 07:48:17','2024-05-02 09:55:01'),(27,'15',8,'2024-04-25 07:48:21','2024-04-25 07:48:21'),(45,'35',9,'2024-04-25 15:50:01','2024-05-02 16:14:05');
 /*!40000 ALTER TABLE `green_houses` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -145,7 +145,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -154,7 +154,7 @@ CREATE TABLE `migrations` (
 
 LOCK TABLES `migrations` WRITE;
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_reset_tokens_table',1),(3,'2014_10_12_100000_create_password_resets_table',1),(4,'2019_08_19_000000_create_failed_jobs_table',1),(5,'2019_12_14_000001_create_personal_access_tokens_table',1),(6,'2024_04_11_073727_create_smart_farms_table',2),(21,'2024_04_11_075127_create_cultivations_table',3),(22,'2024_04_11_075433_create_green_houses_table',3),(23,'2024_04_11_075950_create_owners_table',3),(24,'2024_04_11_083021_create_technologies_table',3),(25,'2024_04_11_083204_create_supplier_companies_table',3),(26,'2024_04_11_083617_create_measures_table',3),(27,'2024_04_11_090139_add_level_column_to_users_table',3),(28,'2024_04_25_150605_add_owner_id_to_smart_farms_table',4),(29,'2024_04_25_155511_add_smart_farm_id_to_green_houses_table',5),(30,'2024_04_25_170853_add_supplier_company_id_to_technologies_table',6),(31,'2024_04_25_174751_add_green_house_id_to_measures_table',7);
+INSERT INTO `migrations` VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_reset_tokens_table',1),(3,'2014_10_12_100000_create_password_resets_table',1),(4,'2019_08_19_000000_create_failed_jobs_table',1),(5,'2019_12_14_000001_create_personal_access_tokens_table',1),(6,'2024_04_11_073727_create_smart_farms_table',2),(21,'2024_04_11_075127_create_cultivations_table',3),(22,'2024_04_11_075433_create_green_houses_table',3),(23,'2024_04_11_075950_create_owners_table',3),(24,'2024_04_11_083021_create_technologies_table',3),(25,'2024_04_11_083204_create_supplier_companies_table',3),(26,'2024_04_11_083617_create_measures_table',3),(27,'2024_04_11_090139_add_level_column_to_users_table',3),(28,'2024_04_25_150605_add_owner_id_to_smart_farms_table',4),(29,'2024_04_25_155511_add_smart_farm_id_to_green_houses_table',5),(30,'2024_04_25_170853_add_supplier_company_id_to_technologies_table',6),(31,'2024_04_25_174751_add_green_house_id_to_measures_table',7),(33,'2024_04_26_101931_create_realized_measures_table',8),(35,'2024_04_30_122633_create_used_technologies_table',9),(36,'2024_04_30_145822_create_realized_crops_table',10);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -190,7 +190,7 @@ CREATE TABLE `owners` (
 
 LOCK TABLES `owners` WRITE;
 /*!40000 ALTER TABLE `owners` DISABLE KEYS */;
-INSERT INTO `owners` VALUES (3,'FLLLEI02R17E410W','Elia','Felletti','2002-10-17','Lagosanto','3383971808','elia.felletti@mail.com','Via Zappaterra','345','Ferrara','44122','2024-04-25 07:58:35','2024-04-25 07:58:35'),(4,'MCCDRA02T31R098R','Dario','Macchi','2002-12-31','Ferrara','3772158909','dario.macchi@mail.com','Via Tizio','6','Sempronio','12345','2024-04-25 13:13:18','2024-04-25 13:13:18');
+INSERT INTO `owners` VALUES (3,'FLLLEI02R17E410W','Elia','Felletti','2002-10-17','Lagosanto','3383971808','elia.felletti@mail.com','Via Zappaterra','345','Ferrara','44122','2024-04-25 07:58:35','2024-04-25 07:58:35'),(4,'MCCDRA02T31D548R','Dario','Macchi','2002-12-31','Ferrara','3772158909','dario.macchi@mail.com','Via Tizio','6','Sempronio','12345','2024-04-25 13:13:18','2024-04-30 10:24:01');
 /*!40000 ALTER TABLE `owners` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -276,6 +276,74 @@ LOCK TABLES `personal_access_tokens` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `realized_crops`
+--
+
+DROP TABLE IF EXISTS `realized_crops`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `realized_crops` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id_proprietario` bigint unsigned DEFAULT NULL,
+  `id_serra` bigint unsigned DEFAULT NULL,
+  `id_coltura` bigint unsigned DEFAULT NULL,
+  `data_semina` date NOT NULL,
+  `data_raccolta_teorica` date NOT NULL,
+  `data_raccolta_effettiva` date DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `realized_crops_id_proprietario_foreign` (`id_proprietario`),
+  KEY `realized_crops_id_serra_foreign` (`id_serra`),
+  KEY `realized_crops_id_coltura_foreign` (`id_coltura`),
+  CONSTRAINT `realized_crops_id_coltura_foreign` FOREIGN KEY (`id_coltura`) REFERENCES `cultivations` (`id`),
+  CONSTRAINT `realized_crops_id_proprietario_foreign` FOREIGN KEY (`id_proprietario`) REFERENCES `owners` (`id`),
+  CONSTRAINT `realized_crops_id_serra_foreign` FOREIGN KEY (`id_serra`) REFERENCES `green_houses` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `realized_crops`
+--
+
+LOCK TABLES `realized_crops` WRITE;
+/*!40000 ALTER TABLE `realized_crops` DISABLE KEYS */;
+INSERT INTO `realized_crops` VALUES (2,3,25,58,'2024-04-30','2024-06-30',NULL,'2024-04-30 13:10:20','2024-04-30 13:10:20'),(3,4,26,59,'2024-04-30','2024-07-30',NULL,'2024-04-30 13:11:21','2024-05-02 12:53:19'),(4,4,45,58,'2024-05-02','2024-07-02',NULL,'2024-05-02 12:53:50','2024-05-02 12:53:50');
+/*!40000 ALTER TABLE `realized_crops` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `realized_measures`
+--
+
+DROP TABLE IF EXISTS `realized_measures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `realized_measures` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id_tecnologia` bigint unsigned DEFAULT NULL,
+  `id_misura` bigint unsigned DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `realized_measures_id_tecnologia_foreign` (`id_tecnologia`),
+  KEY `realized_measures_id_misura_foreign` (`id_misura`),
+  CONSTRAINT `realized_measures_id_misura_foreign` FOREIGN KEY (`id_misura`) REFERENCES `measures` (`id`),
+  CONSTRAINT `realized_measures_id_tecnologia_foreign` FOREIGN KEY (`id_tecnologia`) REFERENCES `technologies` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `realized_measures`
+--
+
+LOCK TABLES `realized_measures` WRITE;
+/*!40000 ALTER TABLE `realized_measures` DISABLE KEYS */;
+INSERT INTO `realized_measures` VALUES (4,2,31,'2024-04-26 10:27:46','2024-04-26 10:27:46'),(5,3,31,'2024-04-26 10:27:53','2024-04-26 10:27:53'),(6,4,31,'2024-04-26 10:27:58','2024-04-26 10:27:58'),(7,5,31,'2024-04-26 10:28:05','2024-04-26 10:28:05'),(8,6,31,'2024-04-26 10:28:10','2024-04-26 10:28:10'),(9,2,32,'2024-04-30 10:13:08','2024-04-30 10:15:38'),(10,3,32,'2024-04-30 10:14:40','2024-04-30 10:14:40'),(11,4,32,'2024-04-30 10:15:06','2024-04-30 10:15:06'),(12,5,32,'2024-04-30 10:15:16','2024-04-30 10:15:16'),(14,6,32,'2024-04-30 10:15:50','2024-04-30 10:15:50'),(15,12,34,'2024-05-02 13:45:58','2024-05-02 13:45:58'),(16,3,34,'2024-05-02 13:46:06','2024-05-02 13:46:06'),(17,4,34,'2024-05-02 13:46:13','2024-05-02 13:46:13'),(18,5,34,'2024-05-02 13:46:26','2024-05-02 13:46:26'),(19,6,34,'2024-05-02 13:46:32','2024-05-02 13:46:32'),(20,12,35,'2024-05-02 13:46:44','2024-05-02 14:04:47'),(21,3,35,'2024-05-02 13:46:51','2024-05-02 13:46:51'),(22,4,35,'2024-05-02 13:46:59','2024-05-02 13:46:59'),(23,5,35,'2024-05-02 13:47:04','2024-05-02 13:47:04'),(29,6,35,'2024-05-02 16:32:04','2024-05-02 16:32:04');
+/*!40000 ALTER TABLE `realized_measures` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `smart_farms`
 --
 
@@ -340,7 +408,7 @@ CREATE TABLE `supplier_companies` (
 
 LOCK TABLES `supplier_companies` WRITE;
 /*!40000 ALTER TABLE `supplier_companies` DISABLE KEYS */;
-INSERT INTO `supplier_companies` VALUES (2,'Azienda_1','azienda.1@mail.com','3491237659','023491237659','Via Roma','10','Roma','12346','2024-04-25 07:50:14','2024-04-25 07:50:14'),(3,'Azienda_2','azienda.2@mail.com','3774567890',NULL,'Via Firenze','9','Firenze','09876','2024-04-25 07:51:14','2024-04-25 07:51:14');
+INSERT INTO `supplier_companies` VALUES (2,'Azienda_1','azienda1@mail.com','3491237659','023491237659','Via Roma','10','Roma','12346','2024-04-25 07:50:14','2024-05-02 14:44:53'),(3,'Azienda_2','azienda2@mail.com','3774567890',NULL,'Via Firenze','9','Firenze','09876','2024-04-25 07:51:14','2024-05-02 14:45:02');
 /*!40000 ALTER TABLE `supplier_companies` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -361,7 +429,7 @@ CREATE TABLE `technologies` (
   PRIMARY KEY (`id`),
   KEY `technologies_id_azienda_fornitrice_foreign` (`id_azienda_fornitrice`),
   CONSTRAINT `technologies_id_azienda_fornitrice_foreign` FOREIGN KEY (`id_azienda_fornitrice`) REFERENCES `supplier_companies` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -370,8 +438,39 @@ CREATE TABLE `technologies` (
 
 LOCK TABLES `technologies` WRITE;
 /*!40000 ALTER TABLE `technologies` DISABLE KEYS */;
-INSERT INTO `technologies` VALUES (2,'Termometro 2.0','Temperatura',2,'2024-04-25 07:51:48','2024-04-25 07:51:48'),(3,'Sonda umidità 1.0','Umidità',2,'2024-04-25 07:52:25','2024-04-25 07:52:25'),(4,'Light sensor 1.0','Luminosità',2,'2024-04-25 07:52:54','2024-04-25 07:52:54'),(5,'Misuratore CO2 1.0','CO2',3,'2024-04-25 07:53:53','2024-04-25 07:53:53'),(6,'Misuratore irrigazione 2.0','Irrigazione',3,'2024-04-25 07:54:41','2024-04-25 07:54:41'),(12,'Termometro 3.0','Temperatura',3,'2024-04-25 15:31:53','2024-04-25 15:45:08');
+INSERT INTO `technologies` VALUES (2,'Termometro 2.0','Temperatura',2,'2024-04-25 07:51:48','2024-04-25 07:51:48'),(3,'Sonda umidità 1.0','Umidità',2,'2024-04-25 07:52:25','2024-04-25 07:52:25'),(4,'Light sensor 1.0','Luminosità',2,'2024-04-25 07:52:54','2024-04-25 07:52:54'),(5,'Misuratore CO2 1.0','CO2',3,'2024-04-25 07:53:53','2024-04-25 07:53:53'),(6,'Misuratore irrigazione 2.0','Irrigazione',3,'2024-04-25 07:54:41','2024-04-25 07:54:41'),(12,'Termometro 3.0','Temperatura',3,'2024-04-25 15:31:53','2024-04-25 15:45:08'),(14,'Termometro 4.0','Temperatura',2,'2024-05-02 15:48:00','2024-05-02 16:37:09');
 /*!40000 ALTER TABLE `technologies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `used_technologies`
+--
+
+DROP TABLE IF EXISTS `used_technologies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `used_technologies` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id_tecnologia` bigint unsigned DEFAULT NULL,
+  `id_smart_farm` bigint unsigned DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `used_technologies_id_tecnologia_foreign` (`id_tecnologia`),
+  KEY `used_technologies_id_smart_farm_foreign` (`id_smart_farm`),
+  CONSTRAINT `used_technologies_id_smart_farm_foreign` FOREIGN KEY (`id_smart_farm`) REFERENCES `smart_farms` (`id`),
+  CONSTRAINT `used_technologies_id_tecnologia_foreign` FOREIGN KEY (`id_tecnologia`) REFERENCES `technologies` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `used_technologies`
+--
+
+LOCK TABLES `used_technologies` WRITE;
+/*!40000 ALTER TABLE `used_technologies` DISABLE KEYS */;
+INSERT INTO `used_technologies` VALUES (1,2,8,'2024-04-30 12:46:01','2024-04-30 12:48:46'),(3,3,8,'2024-04-30 12:46:37','2024-04-30 12:46:37'),(4,4,8,'2024-04-30 12:46:43','2024-04-30 12:46:43'),(5,5,8,'2024-04-30 12:46:48','2024-04-30 12:46:48'),(6,6,8,'2024-04-30 12:46:54','2024-04-30 12:46:54'),(7,12,9,'2024-04-30 12:46:59','2024-04-30 12:46:59'),(8,3,9,'2024-04-30 12:47:03','2024-04-30 12:47:03'),(9,4,9,'2024-04-30 12:47:09','2024-04-30 12:47:09'),(10,5,9,'2024-04-30 12:47:18','2024-04-30 12:47:18'),(12,6,9,'2024-04-30 12:48:57','2024-04-30 12:48:57');
+/*!40000 ALTER TABLE `used_technologies` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -393,7 +492,7 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -402,7 +501,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Elia','elia.felletti@mail.com',NULL,'$2y$10$wOoE0RrEsY/5B5lJY0sLSOFY1V6B80QUnZM2i9nPcV77Hjkm4JcaO',1,NULL,'2024-04-17 05:56:31','2024-04-17 05:56:31'),(2,'admin1','admin1@mail.com',NULL,'$2y$10$N/7pT4pPhsjf//CexOWSn.bBb1X3Pia1Nwg31nN4dDGUzC5auYT0.',0,NULL,'2024-04-17 06:02:39','2024-04-17 06:02:39'),(3,'admin2','admin2@mail.com',NULL,'$2y$10$Qs3XtS5CX.lgFSf2jhtyAOiHoKfQanyAhMKQPZ9t.dHQQ2KiJQTty',0,NULL,'2024-04-17 06:25:46','2024-04-17 06:25:46'),(4,'azienda1','azienda1@mail.com',NULL,'$2y$10$CLCDeQlRj9npqwC2DU8/TuBHEwjDaHP2qvZnMvdratRj122mJuTGe',2,NULL,'2024-04-17 07:42:54','2024-04-17 07:42:54');
+INSERT INTO `users` VALUES (1,'Elia','elia.felletti@mail.com',NULL,'$2y$10$wOoE0RrEsY/5B5lJY0sLSOFY1V6B80QUnZM2i9nPcV77Hjkm4JcaO',1,NULL,'2024-04-17 05:56:31','2024-04-17 05:56:31'),(2,'admin1','admin1@mail.com',NULL,'$2y$10$N/7pT4pPhsjf//CexOWSn.bBb1X3Pia1Nwg31nN4dDGUzC5auYT0.',0,NULL,'2024-04-17 06:02:39','2024-04-17 06:02:39'),(3,'admin2','admin2@mail.com',NULL,'$2y$10$Qs3XtS5CX.lgFSf2jhtyAOiHoKfQanyAhMKQPZ9t.dHQQ2KiJQTty',0,NULL,'2024-04-17 06:25:46','2024-04-17 06:25:46'),(4,'azienda1','azienda1@mail.com',NULL,'$2y$10$CLCDeQlRj9npqwC2DU8/TuBHEwjDaHP2qvZnMvdratRj122mJuTGe',2,NULL,'2024-04-17 07:42:54','2024-04-17 07:42:54'),(5,'Dario','dario.macchi@mail.com',NULL,'$2y$10$PRsSLJiXHV0k6yd2rHnq9.uKmJX1pW2zIhTZz.lRgh96hAfZcxfeC',1,NULL,'2024-05-02 08:26:52','2024-05-02 08:26:52'),(6,'azienda2','azienda2@mail.com',NULL,'$2y$10$tjp5BM5OwpcmUI4A36s72OhJ3jef9j3sisHyf.1QCBAi6QQNI8/f2',2,NULL,'2024-05-02 14:43:22','2024-05-02 14:43:22');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -415,4 +514,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-25 18:04:53
+-- Dump completed on 2024-05-07 16:33:38
